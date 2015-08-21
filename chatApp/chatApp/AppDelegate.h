5d0c3460cb78310be700204722e9790208ffa1a3
Sean Liu <seanliu@Seans-MBP.home>
2015-08-20 22:22:41 -0400

    added chatApp

---
+++ chatApp/chatApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chatApp
+//
+//  Created by Sean Liu on 8/20/15.
+//  Copyright (c) 2015 Sean Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chatApp/chatApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chatApp
+//
+//  Created by Sean Liu on 8/20/15.
+//  Copyright (c) 2015 Sean Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
