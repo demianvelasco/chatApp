60296a94fdc7aeccff04e5047ecab9355c37ac87
Demian Velasco <demian@Demians-Air.home>
2015-08-20 22:29:56 -0400

    Added a button

--- chatApp/chatApp/ViewController.m
+++ chatApp/chatApp/ViewController.m
@@ -19,6 +19,11 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+
+- (IBAction)ButtonAction:(id)sender {
+    NSLog(@"Hello, Sean");
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
